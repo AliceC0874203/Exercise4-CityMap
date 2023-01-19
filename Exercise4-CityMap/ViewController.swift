5dfbd7bd74ff59fd374bd841d45c7e232033a3af
Alice <c0874203@mylambton.ca>
2023-01-18 19:51:53 -0500

    Exercise4-CityMap
    Exercise4-CityMap

--- Exercise4-CityMap/ViewController.swift
+++ Exercise4-CityMap/ViewController.swift
@@ -6,14 +6,163 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
+    // create location manager
+    var locationMnager = CLLocationManager()
+    
+    // destination variable
+    var destination: CLLocationCoordinate2D!
+    
+    var points = [CLLocationCoordinate2D]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // we assign the delegate property of the location manager to be this class
+        locationMnager.delegate = self
+        
+        // we define the accuracy of the location
+        locationMnager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // rquest for the permission to access the location
+        locationMnager.requestWhenInUseAuthorization()
+        
+        // start updating the location
+        locationMnager.startUpdatingLocation()
+        
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(addLongPressAnnotattion))
+        map.addGestureRecognizer(uilpgr)
+        
+        // giving the delegate of MKMapViewDelegate to this class
+        map.delegate = self
     }
+    
+    //MARK: - long press gesture recognizer for the annotation
+    @objc func addLongPressAnnotattion(gestureRecognizer: UIGestureRecognizer) {
+        let touchPoint = gestureRecognizer.location(in: map)
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: map)
+        
+        // add annotation for the coordinatet
+        let annotation = MKPointAnnotation()
+        annotation.title = "my favorite"
+        annotation.coordinate = coordinate
+        map.addAnnotation(annotation)
+        
+        map.removeOverlays(map.overlays)
+        points.append(coordinate)
+        
+        addPolygon()
+        addPolyline()
+    }
+    
+    //MARK: - polyline method
+    func addPolyline() {
+        let polyline = MKPolyline(coordinates: points, count: points.count)
+        map.addOverlay(polyline)
+    }
+    
+    //MARK: - polygon method
+    func addPolygon() {
+        let polygon = MKPolygon(coordinates: points, count: points.count)
+        map.addOverlay(polygon)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+//        print(locations.count)
+        let userLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        
+        displayLocation(latitude: latitude, longitude: longitude, title: "my location", subtitle: "you are here")
+    }
+    
+    //MARK: - display user location method
+    func displayLocation(latitude: CLLocationDegrees,
+                         longitude: CLLocationDegrees,
+                         title: String,
+                         subtitle: String) {
+        // 2nd step - define span
+        let latDelta: CLLocationDegrees = 0.05
+        let lngDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lngDelta)
+        // 3rd step is to define the location
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        // 4th step is to define the region
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        // 5th step is to set the region for the map
+        map.setRegion(region, animated: true)
+        
+        // 6th step is to define annotation
+        let annotation = MKPointAnnotation()
+        annotation.title = title
+        annotation.subtitle = subtitle
+        annotation.coordinate = location
+        map.addAnnotation(annotation)
+    }
+}
 
 
+extension ViewController: MKMapViewDelegate {
+    
+    //MARK: - viewFor annotation method
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        switch annotation.title {
+        case "my location":
+            let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "MyMarker")
+            annotationView.markerTintColor = UIColor.blue
+            return annotationView
+        case "my destination":
+            let annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
+            annotationView.animatesDrop = true
+            annotationView.pinTintColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+            return annotationView
+        case "my favorite":
+            let annotationView = map.dequeueReusableAnnotationView(withIdentifier: "customPin") ?? MKPinAnnotationView()
+            annotationView.image = UIImage(named: "ic_place_2x")
+            annotationView.canShowCallout = true
+            annotationView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            return annotationView
+        default:
+            return nil
+        }
+    }
+    
+    //MARK: - rendrer for overlay func
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let rendrer = MKCircleRenderer(overlay: overlay)
+            rendrer.fillColor = UIColor.black.withAlphaComponent(0.5)
+            rendrer.strokeColor = UIColor.green
+            rendrer.lineWidth = 2
+            return rendrer
+        } else if overlay is MKPolyline {
+            let rendrer = MKPolylineRenderer(overlay: overlay)
+            rendrer.strokeColor = UIColor.blue
+            rendrer.lineWidth = 3
+            return rendrer
+        } else if overlay is MKPolygon {
+            let rendrer = MKPolygonRenderer(overlay: overlay)
+            rendrer.fillColor = UIColor.red.withAlphaComponent(0.6)
+            rendrer.strokeColor = UIColor.yellow
+            rendrer.lineWidth = 2
+            return rendrer
+        }
+        return MKOverlayRenderer()
+    }
 }
 
